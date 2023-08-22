bc9b76672245448b6cfff03af55a1d384eb07a92
idevtier <idetier@gmail.com>
2023-08-22 16:32:44 +0300

    feat(app): change avatar

--- pages/index.jsx
+++ pages/index.jsx
@@ -48,7 +48,7 @@ const Page = () => {
               maxWidth="100px"
               display="inline-block"
               borderRadius="full"
-              src="/images/idevtier.jpg"
+              src="/images/idevtier.png"
             />
           </Box>
         </Box>
