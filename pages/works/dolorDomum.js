c6c1ae2747f9cef0d3ffa0d7425a0bf76776bcc7
suzumenobu <idevtier@gmail.com>
2024-08-11 20:43:10 +0800

    improve: Previous works description

--- pages/works/dolorDomum.js
+++ pages/works/dolorDomum.js
@@ -12,7 +12,7 @@ const Work = () => {
           DolorDomum <Badge>2022</Badge>
         </Title>
         <P>
-          The idea of drawing energy from the environment has nowadays moved from the realm of wishful thinking to the realm of practical implementation
+          The idea of drawing energy from the environment has nowadays moved from the realm of wishful thinking to the realm of practical implementation. So this simple landing increased sales by 14%.
         </P>
         <P>
           I&apos;ve developed simple landing for DolorDomum consultations
